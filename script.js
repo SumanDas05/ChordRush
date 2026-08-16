115ef38a0c1c5ebb217fe6a79e8c70d4a75708da
unknown <sumandaz902@gmail.com>
2026-08-16 21:59:20 +0530

    Initial project setup

---
+++ script.js
@@ -0,0 +1,9 @@
+// Chord Runner - Step 1: Project Setup
+// This file will grow as we add features step by step.
+
+const startBtn = document.getElementById("start-btn");
+
+startBtn.addEventListener("click", () => {
+  console.log("Start Game clicked!");
+  alert("Game will start here soon 🎸");
+});
